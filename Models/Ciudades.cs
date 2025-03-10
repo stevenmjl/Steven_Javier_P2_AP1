64e50dfb66bcc73d3835bb52be5fc81c55494c07
stevenmjl <stevenmjl.24@gmail.com>
2025-03-10 18:23:31 -0400

    Creando modelo Ciudades

---
+++ Models/Ciudades.cs
@@ -0,0 +1,9 @@
+﻿namespace Steven_Javier_P2_AP1.Models
+{
+    public class Ciudades
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public double Monto { get; set; }
+    }
+}
