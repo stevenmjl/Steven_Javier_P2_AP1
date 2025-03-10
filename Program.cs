5f5d31a2e25133d1e868b300e75f6f64c1567fd3
stevenmjl <stevenmjl.24@gmail.com>
2025-03-10 18:37:55 -0400

    Agregando Servicio al Program

--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
+using Microsoft.EntityFrameworkCore;
 using Steven_Javier_P2_AP1.Components;
+using Steven_Javier_P2_AP1.DAL;
+using Steven_Javier_P2_AP1.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+var ConStr = builder.Configuration.GetConnectionString("ConStr");
+builder.Services.AddDbContext<Contexto>(Options => Options.UseSqlite(ConStr));
+builder.Services.AddScoped<Estudiantes>();
 
 var app = builder.Build();
 
