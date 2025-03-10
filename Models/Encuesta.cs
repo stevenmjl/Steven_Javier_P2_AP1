9af379e53551d8903b2ae3960956e6ae4f9ae502
stevenmjl <stevenmjl.24@gmail.com>
2025-03-10 19:27:08 -0400

    Editando modelo Encuesta

--- Models/Encuesta.cs
+++ Models/Encuesta.cs
@@ -1,11 +1,18 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace Steven_Javier_P2_AP1.Models
 {
     public class Encuesta
     {
+        [Key]
         public int Id { get; set; }
+        [Required(ErrorMessage = "Campo no debe quedar vacío.")]
+        [MaxLength(60, ErrorMessage = "No mayor de 60 letras")]
         public string? Asignatura { get; set; }
+        [Required]
         public DateTime Fecha { get; set; }
+        [Required(ErrorMessage = "Debe haber un monto")]
+        [Range(1,200000, ErrorMessage = "Desde 1 hasta 200,000")]
         public double Monto { get; set; }
     }
 }
