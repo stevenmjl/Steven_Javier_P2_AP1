a5a7ce8860968a56ccafe312f3016bcb136caa73
stevenmjl <stevenmjl.24@gmail.com>
2025-03-10 19:16:28 -0400

    Creando Service de Encuesta.

---
+++ Services/EncuestasService.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using Steven_Javier_P2_AP1.DAL;
+using Steven_Javier_P2_AP1.Models;
+
+namespace Steven_Javier_P1_AP1.Services;
+
+public class EncuestaService(IDbContextFactory<Contexto> DbFactory)
+{
+    public async Task<bool> Guardar(Encuesta encuesta)
+    {
+        if (!await Existe(encuesta.Id))
+        {
+            return await Insertar(encuesta);
+        }
+        else
+        {
+            return await Modificar(encuesta);
+        }
+    }
+
+    private async Task<bool> Existe(int encuestaId)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Encuestas
+            .AnyAsync(e => e.Id == encuestaId);
+    }
+
+    private async Task<bool> Insertar(Encuesta encuesta)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        contexto.Encuestas.Add(encuesta);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+
+    private async Task<bool> Modificar(Encuesta encuesta)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        contexto.Update(encuesta);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+
+    public async Task<Encuesta?> Buscar(int encuestaId)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Encuestas
+            .FirstOrDefaultAsync(e => e.Id == encuestaId);
+    }
+
+    public async Task<bool> Eliminar(int encuestaId)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Encuestas
+            .AsNoTracking()
+            .Where(e => e.Id == encuestaId)
+            .ExecuteDeleteAsync() > 0;
+    }
+
+    public async Task<List<Encuesta>> Listar(Expression<Func<Encuesta, bool>> criterio)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Encuestas
+            .Where(criterio)
+            .AsNoTracking()
+            .ToListAsync();
+    }
+}
