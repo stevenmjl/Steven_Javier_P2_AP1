547694bd25cd8d272df1d253da80c4ff6a212489
stevenmjl <stevenmjl.24@gmail.com>
2025-03-10 19:06:41 -0400

    Creando modelo Encuesta

---
+++ Models/Encuesta.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Steven_Javier_P2_AP1.Models
+{
+    public class Encuesta
+    {
+        public int Id { get; set; }
+        public string? Asignatura { get; set; }
+        public DateTime Fecha { get; set; }
+        public double Monto { get; set; }
+    }
+}
--- Models/Estudiantes.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Steven_Javier_P2_AP1.Models
-{
-    public class Estudiantes
-    {
-    }
-}
